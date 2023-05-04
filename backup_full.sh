c2c06db5eb7007b57e4643016edca73ef76b21fc
Hereward Cooper <coops@fawk.eu>
2023-05-04 10:52:09 -0700

    add last_success file

--- backup_full.sh
+++ backup_full.sh
@@ -6,8 +6,9 @@ email=$1
 echo "Starting full sync of $email."
 echo "Report will be sent to $GMVAULT_SEND_REPORTS_TO."
 
-gmvault sync -d /data/$email $GMVAULT_OPTIONS $email 2>&1 |
+(
+  gmvault sync -d /data/$email $GMVAULT_OPTIONS $email 2>&1 |
 	tee /data/${email}/${email}_full.log |
 	mail -s "Mail Backup (full) | $email | $(date +'%Y-%m-%d %r %Z')" $GMVAULT_SEND_REPORTS_TO
-
+) && touch /data/$email/last_success
 echo "Full sync complete."
--- backup_quick.sh
+++ backup_quick.sh
@@ -6,8 +6,9 @@ email=$1
 echo "Starting quick sync of $email."
 echo "Report will be sent to $GMVAULT_SEND_REPORTS_TO."
 
-gmvault sync -t quick -d /data/$email $GMVAULT_OPTIONS $email 2>&1 |
+(
+  gmvault sync -t quick -d /data/$email $GMVAULT_OPTIONS $email 2>&1 |
 	tee /data/${email}/${email}_quick.log |
 	mail -s "Mail Backup (quick) | $email | $(date +'%Y-%m-%d %r %Z')" $GMVAULT_SEND_REPORTS_TO
-
+) && touch /data/$email/last_success
 echo "Quick sync complete."
